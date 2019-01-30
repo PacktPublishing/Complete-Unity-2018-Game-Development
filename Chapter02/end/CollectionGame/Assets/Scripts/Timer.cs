ac9a6b5a23b34fea8c11bfb050930de157e9991b
VishalMewadaPackt <vishalm@packt.com>
2019-01-30 11:22:14 +0530

    Code files added

---
+++ Chapter02/end/CollectionGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿//-------------------------
+using UnityEngine;
+using System.Collections;
+//-------------------------
+public class Timer : MonoBehaviour
+{
+	//-------------------------
+	//Maximum time to complete level (in seconds)
+	public float MaxTime = 60f;
+	//-------------------------
+	//Countdown
+	[SerializeField]
+	private float CountDown = 0;
+	//-------------------------
+	// Use this for initialization
+	void Start () 
+	{
+		CountDown = MaxTime;
+	}
+	//-------------------------
+	// Update is called once per frame
+	void Update () 
+	{
+		//Reduce time
+		CountDown -= Time.deltaTime;
+
+		//Restart level if time runs out
+		if(CountDown <= 0)
+		{
+			//Reset coin count
+			Coin.CoinCount=0;
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+	//-------------------------
+}
+//-------------------------
